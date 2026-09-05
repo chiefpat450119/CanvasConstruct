594b1b54e0b9220c072135718159cc385a224786
Caden Z <w1nter.ish3re@gmail.com>
2026-09-04 20:13:38 -0700

    Count visible pixels

--- scenes/runtime_texture/runtime_texture.gd
+++ scenes/runtime_texture/runtime_texture.gd
@@ -1,24 +1,29 @@
 class_name RuntimeTexture
 extends Node
 
-var img: Image
-var texture: ImageTexture
+var _img: Image
+var _texture: ImageTexture
+
+# num of visible pixels
+var visible_pixels: int
 
 @export var sprite2d: Sprite2D
 
 func init(w: int, h: int):
-	img = Image.create(w, h, false, Image.FORMAT_RGBAF)
-	texture = ImageTexture.create_from_image(img)
-	sprite2d.texture = texture
+	_img = Image.create(w, h, false, Image.FORMAT_RGBAF)
+	_texture = ImageTexture.create_from_image(_img)
+	sprite2d.texture = _texture
 
 func set_pixel(x: int, y: int, color: Color):
-	img.set_pixel(x, y, color)
-	texture.update(img)
+	_img.set_pixel(x, y, color)
+	_texture.update(_img)
+	visible_pixels += 1
 
 func set_pixels(points: Array[Vector2i], color: Color):
 	for p in points:
-		img.set_pixelv(p, color)
-	texture.update(img)
+		_img.set_pixelv(p, color)
+	_texture.update(_img)
+	visible_pixels += points.size()
 
 func get_pixel(x: int, y: int) -> Color:
-	return img.get_pixel(x, y)
+	return _img.get_pixel(x, y)
--- scenes/test.gd
+++ scenes/test.gd
@@ -5,5 +5,5 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	texture.init(100, 100)
-	texture.img.fill(Color.RED)
-	texture.texture.update(texture.img)
+	texture._img.fill(Color.RED)
+	texture._texture.update(texture._img)
