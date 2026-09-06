eaa54c0a4dfe2924fd3cfc9acd917b585371fccf
Caden Z <w1nter.ish3re@gmail.com>
2026-09-06 11:58:48 -0700

    Make drawing phase a bit better

--- scenes/drawing_phase/drawing_ui.gd
+++ scenes/drawing_phase/drawing_ui.gd
@@ -3,6 +3,11 @@ extends Control
 
 signal finish_requested
 
+const DRAWING_COLUMN_CENTER_X: float = 0.25
+const REFERENCE_COLUMN_CENTER_X: float = 0.75
+const CONTENT_CENTER_Y: float = 0.5
+const CONTENT_VERTICAL_OFFSET: float = 80.0
+
 @export var drawing_grid: DrawingGrid
 @export var reference_art: TextureRect
 @export var reference_art_grid_lines : GridLines
@@ -10,10 +15,12 @@ signal finish_requested
 @export var finish_button: Button
 
 var _drawing_timer: Timer
+var _reference_image_size: Vector2
 
 
 func _ready() -> void:
 	finish_button.pressed.connect(finish_requested.emit)
+	resized.connect(_layout_drawing_content)
 
 
 func setup_drawing(
@@ -21,15 +28,17 @@ func setup_drawing(
 	drawing_timer: Timer = null,
 	initial_drawing: Image = null
 ) -> void:
-	# Setup reference iamge
+	# Setup reference image
 	var reference_width := reference_image.get_width()
 	var reference_height := reference_image.get_height()
+	_reference_image_size = Vector2(reference_width, reference_height)
 	reference_art.texture = reference_image
 	reference_art_grid_lines.init(reference_width, reference_height)
 	
 	# Setup drawing area
 	drawing_grid.active_colour = _get_reference_colour(reference_image)
 	drawing_grid.init(reference_width, reference_height)
+	_layout_drawing_content()
 	if is_instance_valid(initial_drawing):
 		drawing_grid.fill_from_drawing(initial_drawing)
 	_drawing_timer = drawing_timer
@@ -45,6 +54,34 @@ func stop_drawing() -> Image:
 
 func get_drawing_grid() -> DrawingGrid:
 	return drawing_grid
+
+
+func _layout_drawing_content() -> void:
+	if _reference_image_size.x <= 0.0 or _reference_image_size.y <= 0.0:
+		return
+	if size.x <= 0.0 or size.y <= 0.0:
+		return
+
+	var drawing_size := drawing_grid.get_combined_minimum_size()
+	drawing_grid.size = drawing_size
+	var reference_scale := minf(
+		drawing_size.x / _reference_image_size.x,
+		drawing_size.y / _reference_image_size.y
+	)
+	var reference_size := _reference_image_size * reference_scale
+	reference_art.size = reference_size
+
+	var drawing_center := Vector2(
+		size.x * DRAWING_COLUMN_CENTER_X,
+		size.y * CONTENT_CENTER_Y + CONTENT_VERTICAL_OFFSET
+	)
+	var reference_center := Vector2(
+		size.x * REFERENCE_COLUMN_CENTER_X,
+		size.y * CONTENT_CENTER_Y + CONTENT_VERTICAL_OFFSET
+	)
+
+	drawing_grid.position = (drawing_center - drawing_size * 0.5).round()
+	reference_art.position = (reference_center - reference_size * 0.5).round()
 
 
 func _get_reference_colour(reference_texture: Texture2D) -> Color:
