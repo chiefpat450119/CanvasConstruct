0e872ec3914e10b6009e5ce053cb4d827a497074
Nicholas Liang <nicholas.liang.2006@gmail.com>
2026-09-05 21:44:34 -0700

    Added background music
    - Added functions to audio manager for playing different background tracks
    - Audio Manager fades out and fades in music when switching songs

--- scenes/test/nicholas_test.gd
+++ scenes/test/nicholas_test.gd
@@ -9,6 +9,13 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	AudioManager.play_SFX(test_sfx, -10)
+	while (true):
+		AudioManager.play_build_music()
+		await get_tree().create_timer(13).timeout
+		AudioManager.play_drawing_music()
+		await get_tree().create_timer(10).timeout
+		AudioManager.play_combat_music()
+		await get_tree().create_timer(30).timeout
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,7 +23,5 @@ func _process(delta: float) -> void:
 	pass
 
 func _unhandled_input(event: InputEvent) -> void:
-	pass
-	#if event.is_action_pressed("Render"):
-		#var image : Image = drawing_system.get_image_from_drawing()
-		#runtime_texture.init_from_image(image)
+	if event.is_action_pressed("Render"):
+		get_tree().change_scene_to_file("res://scenes/test/test_player.tscn")
