09995d5bb4e61e9a5a0ee493cf17a2e876be9c84
Caden Z <w1nter.ish3re@gmail.com>
2026-09-06 09:16:11 -0700

    Connect main menu buttons using script

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -1,5 +1,15 @@
 extends Control
 
+
+@export var start_button: Button
+@export var tutorial_button: Button
+
+
+func _ready() -> void:
+	start_button.pressed.connect(_on_start_pressed)
+	tutorial_button.pressed.connect(_on_tutorial_pressed)
+
+
 func _on_start_pressed() -> void:
 	GameStateManagerInstance.reset_run()
 	GameStateManagerInstance.switch_to_drawing_phase()
