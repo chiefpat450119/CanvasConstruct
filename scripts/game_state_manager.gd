44f6887887eca9d92da5cf097e7974882f1618a8
Caden Z <w1nter.ish3re@gmail.com>
2026-09-05 19:21:11 -0700

    Game state manager keep track of bosses

--- scripts/game_state_manager.gd
+++ scripts/game_state_manager.gd
@@ -2,6 +2,9 @@ class_name GameStateManager
 extends Node
 
 @export var has_completed_drawing_phase: bool = false
+@export var boss_scenes: Array[PackedScene] = []
+
+var next_boss_index: int = 0
 
 
 func can_repair_parts() -> bool:
